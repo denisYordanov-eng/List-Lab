0e97e12ed1ad60c4151aa3aeba0e413ddd20615d
denisYordanov-eng <denisivov437@gmail.com>
2025-04-01 07:24:45 +0300

    Made a task.

---
+++ 01-RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ').Select(int.Parse).ToList();
+
+            var invalidNumbers = new List<int>();
+            var validNumbers = new List<int>();
+            bool flag = false;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    invalidNumbers.Add(numbers[i]);
+                    
+                }
+                else if (numbers[i] > 0)
+                {
+                    validNumbers.Add(numbers[i]);
+                    flag = true;
+                }
+         
+            }
+            if (flag == true)
+            {
+                validNumbers.Reverse();
+                Console.WriteLine(String.Join(" ", validNumbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
