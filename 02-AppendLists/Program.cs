e72c3a6dca6900b79c78e46abc5bc1f62dd703ea
denisYordanov-eng <denisivov437@gmail.com>
2025-04-01 11:43:25 +0300

    Just made a task.

---
+++ 02-AppendLists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split('|').ToList();
+            input.Reverse();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+
+
+                var num = input[i]
+                        .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+                Console.Write(String.Join(" ", num) + " ");
+            } 
+           
+        }
+    }
+}
