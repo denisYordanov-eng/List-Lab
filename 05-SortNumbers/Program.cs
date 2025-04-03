23502c17f0f85d4987585becd24650f089f90f33
denisYordanov-eng <denisivov437@gmail.com>
2025-04-03 07:16:20 +0300

    Just made a task.

---
+++ 05-SortNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ').Select(decimal.Parse)
+                .ToList();
+
+            numbers.Sort();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i == numbers.Count - 1)
+                {
+
+                    Console.Write("{0}", numbers[i]);
+                }
+                else
+                {
+                    Console.Write($"{numbers[i]} <=  ");
+                }
+            }
+        }
+    }
+}
