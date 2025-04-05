da2700aa9e40ba3917d91e63fedf7d3197350624
denisYordanov-eng <denisivov437@gmail.com>
2025-04-05 12:47:21 +0300

    Made a task

---
+++ 04-SplitByWordCasing/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SplitByWordCasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine()
+                //, ; : . ! ( ) " ' \ / [ ] space
+                .Split(' ', ',', ';', ':', '.', '!', '(', ')', '/', '[', ']','\\').ToList();
+            var lowerWord = new List<string>();
+            var mixedWord = new List<string>();
+            var upperWord = new List<string>();
+
+
+            foreach (var word in text)
+            {
+               
+                if (word != "")
+                {
+
+
+                    if (word.All(char.IsLower))
+                    {
+                        lowerWord.Add(word);
+                    }
+
+
+                    else if (word.All(char.IsUpper))
+                    {
+                        upperWord.Add(word);
+                    }
+                    else
+                    {
+                        mixedWord.Add(word);
+                    }
+                }
+            }
+                Console.WriteLine("Lower-case:"+ " " +String.Join(", ", lowerWord));
+                Console.WriteLine("Mixed-case:"+ " " + String.Join(", ", mixedWord));
+                Console.WriteLine("Upper-case:" + " " + String.Join(", ", upperWord));
+            
+
+        }
+
+        }
+    }
