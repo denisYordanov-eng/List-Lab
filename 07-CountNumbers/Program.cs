ed3d4c4a2c6f2d53fa32f1d3d421b74f1fea1fd7
denisYordanov-eng <denisivov437@gmail.com>
2025-04-06 09:35:41 +0300

    Made a task.

---
+++ 07-CountNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_CountNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+           .Split(' ').Select(int.Parse)
+           .ToList();
+
+            numbers.Sort();
+            int counter = 1;
+            int previousNumber = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+
+                if (numbers[i] == previousNumber)
+                {
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine($"{previousNumber} -> {counter}");
+                    counter = 1;
+                    previousNumber = numbers[i];
+                }
+            }
+            Console.WriteLine($"{previousNumber} -> {counter}");
+        }
+    }
+}
